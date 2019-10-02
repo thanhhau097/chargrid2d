8818add2fac90fccef8ca54b23aacfe8624b3705
Lionel <lionel@cinnamon.is>
2019-10-02 16:41:05 +0700

    update onehot encoder

--- loss.py
+++ loss.py
@@ -1,42 +1,102 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 
+# based on:
+# https://github.com/zhezh/focalloss/blob/master/focalloss.py
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=2, alpha=0.25, size_average=True):
+    r"""Criterion that computes Focal loss.
+    According to [1], the Focal loss is computed as follows:
+    .. math::
+        \text{FL}(p_t) = -\alpha_t (1 - p_t)^{\gamma} \, \text{log}(p_t)
+    where:
+       - :math:`p_t` is the model's estimated probability for each class.
+    Arguments:
+        alpha (float): Weighting factor :math:`\alpha \in [0, 1]`.
+        gamma (float): Focusing parameter :math:`\gamma >= 0`.
+        reduction (Optional[str]): Specifies the reduction to apply to the
+         output: ‘none’ | ‘mean’ | ‘sum’. ‘none’: no reduction will be applied,
+         ‘mean’: the sum of the output will be divided by the number of elements
+         in the output, ‘sum’: the output will be summed. Default: ‘none’.
+    Shape:
+        - Input: :math:`(N, C, H, W)` where C = number of classes.
+        - Target: :math:`(N, H, W)` where each value is
+          :math:`0 ≤ targets[i] ≤ C−1`.
+    Examples:
+        >>> N = 5  # num_classes
+        >>> args = {"alpha": 0.5, "gamma": 2.0, "reduction": 'mean'}
+        >>> loss = kornia.losses.FocalLoss(*args)
+        >>> input = torch.randn(1, N, 3, 5, requires_grad=True)
+        >>> target = torch.empty(1, 3, 5, dtype=torch.long).random_(N)
+        >>> output = loss(input, target)
+        >>> output.backward()
+    References:
+        [1] https://arxiv.org/abs/1708.02002
+    """
+
+    def __init__(self, alpha: float, gamma: Optional[float] = 2.0,
+                 reduction: Optional[str] = 'none') -> None:
         super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
-        if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1 - alpha])
-        if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
+        self.alpha: float = alpha
+        self.gamma: torch.Tensor = torch.tensor(gamma)
+        self.reduction: Optional[str] = reduction
+        self.eps: float = 1e-6
 
-    def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
-        # target = target.view(-1, 1)
-        target = target.view(-1, input.size()[1])
+    def forward(  # type: ignore
+            self,
+            input: torch.Tensor,
+            target: torch.Tensor) -> torch.Tensor:
+        if not torch.is_tensor(input):
+            raise TypeError("Input type is not a torch.Tensor. Got {}"
+                            .format(type(input)))
+        if not len(input.shape) == 4:
+            raise ValueError("Invalid input shape, we expect BxNxHxW. Got: {}"
+                             .format(input.shape))
+        if not input.shape[-2:] == target.shape[-2:]:
+            raise ValueError("input and target shapes must be the same. Got: {}"
+                             .format(input.shape, input.shape))
+        if not input.device == target.device:
+            raise ValueError(
+                "input and target must be in the same device. Got: {}" .format(
+                    input.device, target.device))
+        # compute softmax over the classes axis
+        input_soft = F.softmax(input, dim=1) + self.eps
 
-        logpt = F.log_softmax(input, dim=1)
-        logpt = logpt.gather(1, target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
+        # create the labels one hot tensor
+        target_one_hot = one_hot(target, num_classes=input.shape[1],
+                                 device=input.device, dtype=input.dtype)
 
-        if self.alpha is not None:
-            if self.alpha.type() != input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0, target.data.view(-1))
-            logpt = logpt * Variable(at)
+        # compute the actual focal loss
+        weight = torch.pow(torch.tensor(1.) - input_soft,
+                           self.gamma.to(input.dtype))
+        focal = -self.alpha * weight * torch.log(input_soft)
+        loss_tmp = torch.sum(target_one_hot * focal, dim=1)
 
-        loss = -1 * (1 - pt) ** self.gamma * logpt
-        if self.size_average:
-            return loss.mean()
+        if self.reduction == 'none':
+            loss = loss_tmp
+        elif self.reduction == 'mean':
+            loss = torch.mean(loss_tmp)
+        elif self.reduction == 'sum':
+            loss = torch.sum(loss_tmp)
         else:
-            return loss.sum()
+            raise NotImplementedError("Invalid reduction mode: {}"
+                                      .format(self.reduction))
+        return loss
+
+
+def focal_loss(
+        input: torch.Tensor,
+        target: torch.Tensor,
+        alpha: float,
+        gamma: Optional[float] = 2.0,
+        reduction: Optional[str] = 'none') -> torch.Tensor:
+    r"""Function that computes Focal loss.
+    See :class:`~kornia.losses.FocalLoss` for details.
+    """
+    return FocalLoss(alpha, gamma, reduction)(input, target)
 
 
 class ChargridLoss(nn.Module):
@@ -53,7 +113,7 @@ class ChargridLoss(nn.Module):
         :param pred_seg: N x C x H x W
         :param pred_boxmask:
         :param pred_boxcoord:
-        :param gt_seg: N x H x W x C
+        :param gt_seg: N x H x W
         :param gt_boxmask:
         :param gt_boxcoord:
         :return:
@@ -65,8 +125,16 @@ class ChargridLoss(nn.Module):
 
 
 if __name__ == '__main__':
-    x = torch.zeros([1, 10, 64, 64])
-    y = torch.zeros([1, 64, 64, 10], dtype=torch.long)
-    loss = FocalLoss()
-    print(loss(x, y))
+    # x = torch.zeros([1, 10, 64, 64])
+    # y = torch.zeros([1, 64, 64, 10], dtype=torch.long)
+    # # loss = FocalLoss()
+    # # print(loss(x, y))
+    # print(focal_loss(x, y, 0.25, 2, 'mean'))
+    N = 5  # num_classes
+    loss = FocalLoss(0.25, 2, 'mean')
+    input = torch.randn(1, N, 3, 5, requires_grad=True)
+    target = torch.empty(1, 3, 5, dtype=torch.long).random_(N)
+    output = loss(input, target)
+    print(output)
+    output.backward()
 
